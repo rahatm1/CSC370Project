88cf58940ce54743c2616ca4fdefd2714c4148cd
Alex <alexjlam@uvic.ca>
2016-03-15 14:00:30 -0700

    windows build

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-javac -d ../apache-tomcat-6.0.18/webapps/flight/WEB-INF/classes -classpath ../apache-tomcat-6.0.18/lib/servlet-api.jar:ojdbc6.jar *.java
-xcopy web/*.html 
-cd ../apache-tomcat-6.0.18/bin/ && startup.bat
+javac -d ..\apache-tomcat-6.0.18\webapps\flight\WEB-INF\classes -classpath ..\apache-tomcat-6.0.18\lib\servlet-api.jar;ojdbc6.jar *.java
+xcopy "web\*.html" "..\apache-tomcat-6.0.18\webapps\flight\" /y
+..\apache-tomcat-6.0.18\bin\startup.bat
