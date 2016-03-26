16c7a869e01054ee84a4a0061e6c9be25f834e85
Ushanth Logan <ushanthloganathan@w134-87-189-203.wireless.uvic.ca>
2016-03-26 16:30:02 -0700

    Deletion is done

---
+++ main/DeleteRoute.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class DeleteRoute extends HttpServlet {
+    void processRequest(HttpServletRequest request, HttpServletResponse response)
+    						throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String Route = request.getParameter("route");
+
+        String statementString =
+		"Delete Route where rnum=" +Route;
+        System.out.println(statementString);
+        Connection conn = ConnectionManager.getInstance().getConnection();
+        
+		try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(statementString);
+            stmt.close();
+            out.println("Insertion Successful!");
+        }
+        catch(SQLException e) { out.println(e); }
+        ConnectionManager.getInstance().returnConnection(conn);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    			throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    public String getServletInfo() {  return "DeleteRoute"; }
+}
