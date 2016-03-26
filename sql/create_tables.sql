128e95a76270dc1ad636fbf8848dca1565907ee2
Rahat Mahbub <rahatm1@uvic.ca>
2016-03-26 13:27:58 -0700

    Tables updated

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,50 +1,48 @@
 CREATE TABLE Airline (
-    air_name VARCHAR(255),
-    code CHAR(4) PRIMARY KEY,
+    name VARCHAR(255),
+    acode CHAR(4) PRIMARY KEY,
     website VARCHAR(255)
 );
 
 CREATE TABLE Route (
-    r_number INT CHECK(r_number < 10000) PRIMARY KEY
+    rnum INT CHECK(rnum < 10000) PRIMARY KEY,
+    planemodel CHAR(30)
 );
 
-CREATE TABLE flight (
-    plane_model CHAR(20),
-    code CHAR(4),
-    r_number INT,
-    FOREIGN KEY(code) REFERENCES Airline(code),
-    FOREIGN KEY(r_number) REFERENCES Route(r_number) ON DELETE CASCADE
+CREATE TABLE Operate (
+    acode CHAR(4),
+    rnum INT,
+    FOREIGN KEY(acode) REFERENCES Airline(acode),
+    FOREIGN KEY(rnum) REFERENCES Route(rnum) ON DELETE CASCADE
 );
 
-CREATE TABLE Outgoing (
+CREATE TABLE OutgoingRoutes (
     destination VARCHAR(255),
     outT DATE,
-    r_number INT PRIMARY KEY REFERENCES Route(r_number) ON DELETE CASCADE
+    rnum INT PRIMARY KEY REFERENCES Route(rnum) ON DELETE CASCADE
 );
 
-CREATE TABLE Incoming (
-    origin VARCHAR(255),
+CREATE TABLE IncomingRoutes (
+    source VARCHAR(255),
     incT DATE,
-    r_number INT PRIMARY KEY REFERENCES Route(r_number) ON DELETE CASCADE
+    rnum INT PRIMARY KEY REFERENCES Route(rnum) ON DELETE CASCADE
 );
 
 CREATE TABLE Departure (
     depID INT PRIMARY KEY,
-    gate CHAR(3),
     DepT Date,
-    r_number INT REFERENCES Outgoing(r_number) ON DELETE CASCADE
+    rnum INT REFERENCES Outgoing(rnum) ON DELETE CASCADE
 );
 
 CREATE TABLE Arrival (
     arrID INT PRIMARY KEY,
-    gate CHAR(3),
     arrT Date,
-    r_number INT REFERENCES Incoming(r_number) ON DELETE CASCADE
+    rnum INT REFERENCES Incoming(rnum) ON DELETE CASCADE
 );
 
 CREATE TABLE Gates (
     gate CHAR(3) PRIMARY KEY,
-    is_free CHAR(1)
+    is_free CHAR(1) check (is_free in ('y', 'n'))
 );
 
 CREATE TABLE GateDep (
@@ -58,21 +56,17 @@ CREATE TABLE GateArr (
 );
 
 CREATE TABLE Passengers (
-    passenger_name VARCHAR(255),
+    pID INT,
+    name VARCHAR(255),
     dob DATE,
-    birth_place VARCHAR(255),
-    passId CHAR(20) PRIMARY KEY
+    pob VARCHAR(255),
+    gov_issued_id CHAR(20) PRIMARY KEY,
+    DP INT REFERENCES Departure(depID),
+    AP INT REFERENCES Arrival(arrID)
 );
 
 CREATE TABLE Baggage (
-    baggageID INT PRIMARY KEY,
-    passID CHAR(20) REFERENCES Passengers(passID)
-);
-
-CREATE TABLE Travel (
-    tID INT PRIMARY KEY,
-    freshID INT,
-    depID INT REFERENCES Departure(depID),
-    arrID INT REFERENCES Arrival(arrID),
-    passID CHAR(20) REFERENCES Passengers(passID)
+    bID INT PRIMARY KEY,
+    weight NUMBER,
+    pID CHAR(20) REFERENCES Passengers(passID)
 );
