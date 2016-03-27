e7583d5a186dad5a06fc39d4f4f5686822cfb4fa
Rahat Mahbub <rahatm1@uvic.ca>
2016-03-27 13:30:38 -0700

    fixed scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,5 @@
 javac -d ..\..\apache-tomcat-6.0.18\webapps\flight\WEB-INF\classes -classpath ..\..\apache-tomcat-6.0.18\lib\servlet-api.jar;..\lib\ojdbc6.jar ..\main\*.java
 xcopy "..\web\*.html" "..\..\apache-tomcat-6.0.18\webapps\flight\" /y
 xcopy "..\web\web.xml" "..\..\apache-tomcat-6.0.18\webapps\flight\WEB-INF\" /y
+xcopy "..\web\background.jpg" "..\..\apache-tomcat-6.0.18\webapps\flight\" /y
 ..\..\apache-tomcat-6.0.18\bin\startup.bat
